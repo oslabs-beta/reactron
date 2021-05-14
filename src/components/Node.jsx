177d929337cd97cbc70d3db8dd1b67cb3b38f018
Jimmy <odylic.lin@gmail.com>
2021-05-14 13:25:09 -0400

    added components

---
+++ src/components/Node.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Node() {
+  return <div className='node'>Node</div>;
+}
---
+++ src/components/RenderedPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import ComponentTree from './ComponentTree';
+import Visualizer from './Visualizer';
+
+// Page that will show once directory has been imported
+export default function RenderedPage() {
+return (
+    <div className='renderedPage'>
+      <Header />
+      <ComponentTree />
+      <Visualizer />
+    </div>
+  );
+}
+
+
