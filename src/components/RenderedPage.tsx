c27229291140e0272b1fc7bfaa4473c23322c7aa
Nathaniel A <ncarmstrong92@gmail.com>
2021-05-14 09:04:55 -0400

    Adding webpack update

--- src/components/RenderedPage.tsx
+++ src/components/RenderedPage.tsx
@@ -5,22 +5,8 @@ import Visualizer from './Visualizer';
 
 // Page that will show once directory has been imported
 export default function RenderedPage() {
-<<<<<<< HEAD
-  // allows async rendering, to listen from the main processor
-  ipcRenderer.on('file-opened', (event: any, content: string) => {
-    // reassign #hello to content rendered from ipcRenderer, the file uploaded
-    console.log(content);
-    document.getElementById('tester').innerHTML = content;
-  });
-  return (
-    <div className='renderedPage'>
-      {/* button selectMe in renderer.ts */}
-      <button id='selectMe'>Click Me</button>
-      <iframe id="tester"></iframe>
-=======
 return (
     <div className='renderedPage'>
->>>>>>> cdd30d08009d87f11450dd8edb7d455ea2e55c45
       <Header />
       <ComponentTree />
       <Visualizer />
