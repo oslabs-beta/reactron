99d2dca9665757503700bab47deed4acec106e01
kerri <75552647+kerriannercrawford@users.noreply.github.com>
2021-05-17 15:42:22 -0400

    Successfully locating and logging fiber nodes.

--- puppeteer.js
+++ puppeteer.js
@@ -1,40 +1,92 @@
 const puppeteer = require('puppeteer');
 
+// Inspiration for fiber walk taken with love from ReactION.
 
-module.exports = async function getRoot (url){
-  let contents = ''
-  console.log('hi from puppeteer')
-  const browser = await puppeteer.launch()
+module.exports = async function getRoot(url) {
+  const browser = await puppeteer.launch({ headless: true, devTools: true });
   const page = await browser.newPage();
+  page.setUserAgent(
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+  );
   await page.goto(url);
-  // return console.log(await page.content())
-  contents = await page.content()
-  // console.log(contents)
-  const body = await page.evaluate(async () => {
-    const domElements = document.querySelector('div').innerText;
-    // for (let elem of domElements){
-    //   console.log('this is an element', elem)
-    // }
-    // console.log(typeof domElements)
-    return domElements
-})
-//   //   const hi = 'hi'
-  //   return thing
-  // })
-  // return body
-  return body
-  // const reactEntry = await openedPage.evaluate((contents) => {
-  //   let body = contents.getElementById('body')
-  //   return body
-    // const _entry = () => {
-    //   const DOMElements = document.querySelector('body').children;
-    //   for (let node of DOMElements) {
-    //     if (node._reactRootContainer) {
-    //       return node._reactRootContainer._internalRoot.current; 
-    //     }
-    //   }
-    // }
-  // });
-  // return _entry
-  // await browser.close();
+  await page.waitForSelector('body');
+
+  const nodeData = page
+    .evaluate(async () => {
+      // Locates the root React node and returns
+      const _rootNode = (() => {
+        // Finds all children of body tag
+        const elems = document.querySelector('body').children;
+        for (let el of elems) {
+          if (el._reactRootContainer) {
+            // Returns root React node
+            return el._reactRootContainer._internalRoot.current;
+          }
+        }
+      })();
+
+      // Function that accepts root node and returns whole application's node data
+      function fiberSearch(entry) {
+        let dataArr = [],
+          mainID = 1;
+        // Recursive function to traverse fiber tree passing in rootNode
+        // Iterates through each node's children and siblings and passes applicable
+        // data into dataArr, and returns dataArr
+        function traversal(root, level, parentId) {
+          // if Element has a sibling, recursively calls traversal again
+          if (root.sibling !== null) {
+            mainID += 1;
+            dataArr.push({
+              name: root.sibling,
+              level: `${level}`,
+              id: `${mainID}`,
+              parentId: `${parentId}`,
+              props: Object.keys(root.sibling.memoizedProps),
+            });
+            traversal(root.sibling, level, parentId);
+          }
+          // if Element has a child, recursively calls traversal again
+          if (root.child !== null) {
+            parentId += 1;
+            mainID += 1;
+            dataArr.push({
+              name: root.child,
+              level: `${level}`,
+              id: `${mainID}`,
+              parentId: `${parentId}`,
+              display: 'none',
+              props: Object.keys(root.child.memoizedProps),
+            });
+            traversal(root.child, level + 1, parentId);
+          }
+        }
+        traversal(entry, 0, 0);
+        // Extracts the type name of each fiber node
+        dataArr.forEach((el) => {
+          console.log(el);
+          if (typeof el.name.type === null) {
+            el.name = '';
+          } else if (typeof el.name.type === 'function' && el.name.type.name) {
+            el.name = el.name.type.name;
+          } else if (typeof el.name.type === 'function') {
+            el.name = 'function';
+          } else if (typeof el.name.type === 'object') {
+            el.name = 'function';
+          } else if (typeof el.name.type === 'string') {
+            el.name = el.name.type;
+          }
+          console.log(el);
+        });
+
+        // Setting root parent to an empty string
+        dataArr[0].parentId = '';
+
+        return dataArr;
+      }
+      return fiberSearch(_rootNode);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return nodeData;
 };
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const path = require('path');
 
-const getRoot = require('./puppeteer.js')
+const getRoot = require('./puppeteer.js');
 
-getRoot('http://localhost:3000')
-.then(result => {
+getRoot('http://localhost:5000').then((result) => {
   // result.forEach(el => {
   //   console.log("this is a getRoote in server el", el)
   // })
-   console.log(result)
-})
+  console.log(result);
+});
 
 const fileSysRouters = require('./routers/fileSysRouters.js');
 const bodyParser = require('body-parser');
-
 
 // For Main Server
 const MAIN_PORT = 3000;
@@ -58,6 +56,10 @@ iframeApp.use(express.json());
 // get path needs decided
 iframeApp.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './userInfo/test.html'));
+});
+
+iframeApp.get('/api', (req, res) => {
+  res.send(200);
 });
 
 iframeApp.use((err, req, res, next) => {
