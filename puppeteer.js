c9a478009505aaf169ff891b9db174fd2bed1698
kerri <75552647+kerriannercrawford@users.noreply.github.com>
2021-05-18 14:26:05 -0400

    Working on returning correct data from fiber traversal.

--- puppeteer.js
+++ puppeteer.js
@@ -1,13 +1,7 @@
 const puppeteer = require('puppeteer');
 
-// Inspiration for fiber walk taken with love from ReactION.
-
-// React Fiber Tree needs:
-// - name
-// - children
-
 module.exports = async function getRoot(url) {
-  const browser = await puppeteer.launch({ headless: true, devTools: true });
+  const browser = await puppeteer.launch({ headless: false, devTools: true });
   const page = await browser.newPage();
   page.setUserAgent(
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
@@ -29,6 +23,96 @@ module.exports = async function getRoot(url) {
         }
       })();
 
+      function parentFinder(node) {
+        if (!node.return) return;
+        if (node.return.tag === 0 || node.return.tag === 1) {
+          console.log(`This is the parent's name: ${node.return.type.name}`);
+          return node.return.type.name;
+        } else {
+          return parentFinder(node.return);
+        }
+      }
+
+      const objColors = [
+        'cyan',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'indigo',
+        'violet',
+      ];
+
+      // Math.floor(Math.random() * objColors.length)
+      function findColor() {
+        return objColors[Math.floor(Math.random() * objColors.length)];
+      }
+
+      const testObj = { App: { parent: null, children: [] } };
+      const rootObj = {
+        name: 'App',
+        color: findColor(),
+        pathProps: { className: findColor() },
+        textProps: { x: -25, y: 25 },
+        children: [],
+      };
+
+      // class Node
+      class Node {
+        constructor(name, parent) {
+          (this.name = name),
+            (this.parent = parent),
+            (this.children = []),
+            (this.color = findColor()),
+            (this.pathProps = { className: findColor() }),
+            (this.textProps = { x: -25, y: 25 });
+        }
+      }
+
+      function fiberFinder(node) {
+        if (node.sibling !== null) {
+          if (node.sibling.type.name) {
+            const parent = parentFinder(node.sibling);
+            testObj[node.sibling.type.name] = {
+              parent,
+              children: [],
+            };
+          }
+          fiberFinder(node.sibling);
+        }
+        if (node.child !== null) {
+          if (node.child.type.name)
+            console.log(`This node's name is ${node.child.type.name}`);
+          fiberFinder(node.child);
+        }
+      }
+      fiberFinder(_rootNode);
+      console.log(testObj);
+
+      for (const key in testObj) {
+        // Author
+        if (testObj[key].parent) {
+          // Author . parent = Blog
+          const parent = testObj[key].parent;
+          // push value held to obj[key].children
+          // Blog.children.push(Author)
+          testObj[parent].children.push(key);
+        }
+      }
+
+      for (const key in testObj) {
+        const array = testObj[key].children;
+        for (let i = 0; i < array.length; i += 1) {
+          // array[i] === Blog
+          const tempVar = array[i];
+          array[i] = { name: tempVar, children: testObj[array[i]].children };
+          // array[i] = {Blog: parent: App, children: []}
+        }
+      }
+
+      //console.log(testObj.App);
+
       // Function that accepts root node and returns whole application's node data
       function fiberSearch(entry) {
         let dataArr = [],
@@ -39,11 +123,6 @@ module.exports = async function getRoot(url) {
         // data into dataArr, and returns dataArr
 
         function traversal(root, level, parentId) {
-          // if Element has a sibling, recursively calls traversal again
-          console.log('the root itself: ', root);
-          console.log('the root sibling: ', root.sibling);
-          console.log('the root child: ', root.child);
-          console.log('the root child name: ', root.child.type.name);
           if (root.sibling !== null) {
             mainID += 1;
             dataArr.push({
@@ -72,32 +151,27 @@ module.exports = async function getRoot(url) {
         }
         traversal(entry, 0, 0);
         // Extracts the type name of each fiber node
-        const newArr = [];
 
         dataArr.forEach((el) => {
           if (typeof el.name.type === null) {
             el.name = '';
           } else if (typeof el.name.type === 'function' && el.name.type.name) {
             // Grabs name of root if it is a React component
-            newArr.push(el);
-            console.log(el.name.type.name);
             el.name = el.name.type.name;
           } else if (typeof el.name.type === 'function') {
             el.name = 'function';
           } else if (typeof el.name.type === 'object') {
             el.name = 'function';
           } else if (typeof el.name.type === 'string') {
-            console.log(el.name.type);
             el.name = el.name.type;
           }
         });
-        console.log(newArr);
         // Setting root parent to an empty string
         dataArr[0].parentId = '';
 
         return dataArr;
       }
-      return fiberSearch(_rootNode);
+      //return fiberSearch(_rootNode);
     })
     .catch((err) => {
       console.log(err);
