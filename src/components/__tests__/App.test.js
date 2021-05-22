456d5cb1320f0a3e574b1ef7a5825944c7f6f0a2
Jimmy <odylic.lin@gmail.com>
2021-05-22 17:14:52 -0400

    Updated Jest.config.js, Added jest-css-modules package and updated .babelrc preset env to allow importing for jest testing

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import {render, screen} from '@testing-library/react';
+import HeadNode from '../HeadNode';
+import MainContainer from '../MainContainer'
+import App from '../App'
+
+test('test', () => {
+  expect(true).toBe(true);
+});
