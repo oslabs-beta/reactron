0c41e42b815bfdd44593e346caea25f1748d8dd9
Seymour-creates <romelogilbert@yahoo.com>
2021-05-28 14:04:44 -0400

    pulling latest main

--- puppeteer.js
+++ puppeteer.js
@@ -5,12 +5,20 @@ module.exports = async function getRoot (url){
   let contents = ''
   console.log('hi from puppeteer')
   const browser = await puppeteer.launch()
-  const page = await browser.newPage();
+  const page = await browser.pages()
+    .then(pageArr => {
+      return pageArr[0];
+    });
   await page.goto(url);
-  // return console.log(await page.content())
-  contents = await page.content()
-  // console.log(contents)
-  const body = await page.evaluate(async () => {
+  
+  
+  
+  const reactData = page.evaluate(async () => {
+    const entry = (() => {
+      const domEle = document.getElementById('root').children;
+      console.log(domEle);
+
+    })()
     const domElements = document.querySelector('div').innerText;
     // for (let elem of domElements){
     //   console.log('this is an element', elem)
@@ -18,23 +26,5 @@ module.exports = async function getRoot (url){
     // console.log(typeof domElements)
     return domElements
 })
-//   //   const hi = 'hi'
-  //   return thing
-  // })
-  // return body
-  return body
-  // const reactEntry = await openedPage.evaluate((contents) => {
-  //   let body = contents.getElementById('body')
-  //   return body
-    // const _entry = () => {
-    //   const DOMElements = document.querySelector('body').children;
-    //   for (let node of DOMElements) {
-    //     if (node._reactRootContainer) {
-    //       return node._reactRootContainer._internalRoot.current; 
-    //     }
-    //   }
-    // }
-  // });
-  // return _entry
-  // await browser.close();
+
 };
