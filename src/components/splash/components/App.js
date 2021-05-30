f2dbe90bb79c19074947158bbdde5f911c40dd90
Jimmy <odylic.lin@gmail.com>
2021-05-30 10:52:48 -0400

    Formated the Rendered Page

--- src/components/splash/components/App.js
+++ src/components/splash/components/App.js
@@ -3,15 +3,15 @@ import {useState} from 'react';
 import MainContainer from './MainContainer';
 import AppContainer from '../../AppContainer.jsx';
 // import username from '/Users/kerricrawford/Desktop/coding/production-project/reactron/userInfo/currUser.js';
-// import LoginPage from '../../login/LoginPage.js'
-// import RenderedPage from '../../RenderedPage.jsx'
+import LoginPage from '../../login/LoginPage.js'
+import RenderedPage from '../../RenderedPage.jsx'
 import LandingPage from '../../LandingPage.jsx'
 
 
 function App() {
   const [view, useView] = useState('splash');
   // const [user, useUser] = useState(username);
-  if (view === 'splash') return <LandingPage useView={useView} />;
+  if (view === 'splash') return <RenderedPage useView={useView} />;
   else return <AppContainer />;
 }
 
