a7f43f387179cccdd1d5322979cebf8a8eee3022
SteeleCoale <lcoale@gmail.com>
2021-06-01 11:28:59 -0400

    secrethtml page on main branch

--- userInfo/individualComponent/index.js
+++ userInfo/individualComponent/index.js
@@ -1,1 +1,1 @@
-import React from 'react'; import ReactDOM from 'react-dom'; import Task from '../sample/sampleApp/Task.js'; ReactDOM.render(<Task />, document.getElementById('root'))
+import React from 'react'; import ReactDOM from 'react-dom'; import Body from '../sample/sampleApp/Body.js'; ReactDOM.render(<Body />, document.getElementById('root'))
