dc1a97800bcc73fcdc539daac57c5cf41a9af767
Jimmy <odylic.lin@gmail.com>
2021-06-01 22:47:18 -0400

    Added Styling to FileExplorer Buttons and added formatting to RenderedPage

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function File(props) {
   return (
-    <button onClick={() => props.renderFile(props.name)}>{props.name}</button>
+    <button onClick={() => props.renderFile(props.name)}
+      className="FileExplorerButton"
+    >{props.name}</button>
   );
 }
+
+
